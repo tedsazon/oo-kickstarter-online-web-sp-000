4a8c5223dddce2cbcfbc1959e7a214c44ab3c596
Ted Sazon <tedsazon@gmail.com>
2019-01-07 20:44:48 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,12 +1,13 @@
 class Project
-  attr_accessor :title, :backers
+  attr_accessor :title, :backer
 
   def initialize(title)
     @title = title
     @backers = []
   end
 
-
+  def add_backer(backer)
+    @backers << backer
 
 
 end
